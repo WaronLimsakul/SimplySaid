e5824a29e1a321d97c2e124126f68475cbd2be4a
Waron Limsakul <poonro@gmail.com>
2024-12-23 23:35:10 -0500

    add comment at GET all users

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,8 @@
 import { db } from "@/lib/mongodb";
 
+// get all users
 export async function GET() {
-  const result = await db.collection("users").find().toArray();
-  if (!result) return new Response("Internal server error", { status: 500 });
-  return Response.json(result);
+    const result = await db.collection("users").find().toArray();
+    if (!result) return new Response("Internal server error", { status: 500 });
+    return Response.json(result);
 }
