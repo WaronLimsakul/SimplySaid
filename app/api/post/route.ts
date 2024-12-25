70f5d3bb64f89a76245d29ae66007affa8b440ef
Waron Limsakul <poonro@gmail.com>
2024-12-25 16:54:35 -0500

    modify GET post to fetch randomly when no query param exist

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -6,64 +6,69 @@ const posts_coll = db.collection("posts");
 
 // can get posts by user_id, post_id, tags, object.
 // (Next.js GET method strip the body out, so we use query params instead.)
+// If no query params, fetch from top.
 export async function GET(req: NextRequest) {
-    // create URL object = easy to get params query.
-    const url = new URL(req.url);
-    const search_params = url.searchParams;
-    const tags = search_params.getAll("tags");
+  // create URL object = easy to get params query.
+  const url = new URL(req.url);
+  const search_params = url.searchParams;
+  const tags = search_params.getAll("tags");
 
-    let filter;
-    // find ONE post by its id, first priority
-    if (search_params.has("post_id")) {
-        const result = await posts_coll.findOne({
-            _id: new ObjectId(search_params.get("post_id")),
-        });
-        return Response.json(result);
-        // next priority is the object.
-    } else if (search_params.has("object"))
-        filter = { object: search_params.get("object") };
-    // by user
-    else if (search_params.has("user_id"))
-        filter = { user_id: search_params.get("user_id") };
-    // just contain all tags we want, don't care other tags or order.
-    else if (search_params.has("tags")) filter = { tags: { $all: tags } };
-    else {
-        return new Response(
-            "Can't search by anything other than post id, user id, an object, and tags",
-            { status: 400 },
-        );
-    }
-
-    const result = await posts_coll.find(filter).toArray();
+  let filter;
+  // if there is no query params, search randomly
+  if (!url.search) {
+    filter = {};
+  }
+  // find ONE post by its id, first priority
+  else if (search_params.has("post_id")) {
+    const result = await posts_coll.findOne({
+      _id: new ObjectId(search_params.get("post_id")),
+    });
     return Response.json(result);
+    // next priority is the object.
+  } else if (search_params.has("object"))
+    filter = { object: search_params.get("object") };
+  // by user
+  else if (search_params.has("user_id"))
+    filter = { user_id: search_params.get("user_id") };
+  // just contain all tags we want, don't care other tags or order.
+  else if (search_params.has("tags")) filter = { tags: { $all: tags } };
+  else {
+    return new Response(
+      "Can't search by anything other than post id, user id, an object, and tags",
+      { status: 400 },
+    );
+  }
+
+  const result = await posts_coll.find(filter).toArray();
+  return Response.json(result);
 }
 
 // post need object, title, tags, content + user_id to ensure user is logged in.
 export async function POST(req: Request) {
-    // already check if user logged in from client-side.
-    const { post, user_id }: { post; user_id: string } = await req.json();
-    if (!(post && user_id)) return new Response("Bad request", { status: 400 });
-    const {
-        object,
-        title,
-        tags,
-        content,
-    }: {
-        object: string;
-        title: string;
-        tags: string[];
-        content: string;
-    } = post;
-    if (!(object && title && tags && content))
-        return new Response("Bad request, post data not complete", { status: 400 });
-    const result = await posts_coll.insertOne({
-        object,
-        title,
-        tags,
-        content,
-        // [up, down]
-        votes: [0, 0],
-        user_id,
-    });
-    return Response.json(result);
+  // already check if user logged in from client-side.
+  const { post, user_id }: { post; user_id: string } = await req.json();
+  if (!(post && user_id)) return new Response("Bad request", { status: 400 });
+  const {
+    object,
+    title,
+    tags,
+    content,
+  }: {
+    object: string;
+    title: string;
+    tags: string[];
+    content: string;
+  } = post;
+  if (!(object && title && tags && content))
+    return new Response("Bad request, post data not complete", { status: 400 });
+  const result = await posts_coll.insertOne({
+    object,
+    title,
+    tags,
+    content,
+    // [up, down]
+    votes: [0, 0],
+    user_id,
+  });
+  return Response.json(result);
 }
