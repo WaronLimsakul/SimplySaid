3b621cfde6d40bd0c25e035eca1e7a04788a0ac8
Waron Limsakul <poonro@gmail.com>
2024-12-28 21:13:19 -0500

    add comment on getSession()

--- utils/frontend/get_session.ts
+++ utils/frontend/get_session.ts
@@ -1,7 +1,8 @@
 import { auth } from "@/auth";
 import { cache } from "react";
 
+// this one makes await auth() to be like fetch(.., {no_store})
 export const getSession = cache(async () => {
-    const session = await auth();
-    return session;
+  const session = await auth();
+  return session;
 });
