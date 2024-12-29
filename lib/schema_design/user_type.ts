9511a68940fc44758c82359bed23f7341d269007
Waron Limsakul <poonro@gmail.com>
2024-12-28 21:12:58 -0500

    modify user type

--- lib/schema_design/user_type.ts
+++ lib/schema_design/user_type.ts
@@ -2,12 +2,12 @@ import { ObjectId } from "mongodb";
 
 // from user type
 type usert = {
-    id: null | string | ObjectId;
-    name: string;
-    email: string;
-    image: string;
-    votes: { post_id: string | ObjectId[]; val: 1 | -1 }[] | null;
-    posts: ObjectId[] | string[];
+  id: null | string | ObjectId;
+  name: string;
+  email: string;
+  image: string;
+  votes?: { post_id: string | ObjectId[]; val: 1 | -1 }[];
+  posts?: ObjectId[] | string[];
 };
 // note that posts is objectId[] in the schema but when fetched, from db -> server -> client
 // It changes to string[].
