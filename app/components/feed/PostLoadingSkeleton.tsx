d1ed893583490f6ca27a08093d5a6cadc20356ed
Waron Limsakul <poonro@gmail.com>
2025-01-03 14:27:20 -0500

    Finish skeleton loading post ui

--- app/components/feed/PostLoadingSkeleton.tsx
+++ app/components/feed/PostLoadingSkeleton.tsx
@@ -6,26 +6,23 @@ const PostLoadingSkeleton = () => {
             <div className="md:col-span-3 col-span-0" />
             <div className="col-span-12 md:col-span-6 mx-2 md:mx-0">
                 {[1, 2, 3].map((val) => (
-                    <div key={val} className="my-4">
-                        <Skeleton className="h-10 w-auto my-1 rounded-xl" />
+                    <div key={val} className="my-4 bg-zinc-200 p-4 rounded-lg">
+                        <Skeleton className="h-9 w-3/4 my-1 rounded-xl" />
                         <div className="flex">
-                            <Skeleton className="h-10 w-10 mr-2 my-2 rounded-full" />
-                            <Skeleton className="h-5 w-1/2 my-auto rounded-xl" />
+                            <Skeleton className="h-7 w-7 mr-2 my-2 rounded-full" />
+                            <Skeleton className="h-4 w-1/2 my-auto rounded-xl" />
                         </div>
                         <div className="flex">
-                            <Skeleton className="h-5 w-1/6 rounded-xl mx-2" />
-                            <Skeleton className="h-5 w-1/6 rounded-xl mx-2" />
-                            <Skeleton className="h-5 w-1/6 rounded-xl mx-2" />
+                            <Skeleton className="h-4 w-1/6 rounded-xl mx-2" />
+                            <Skeleton className="h-4 w-1/6 rounded-xl mx-2" />
+                            <Skeleton className="h-4 w-1/6 rounded-xl mx-2" />
                         </div>
                         <div className="pt-3 pb-2">
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
-                            <Skeleton className="h-5 w-full my-2 rounded-xl" />
+                            <Skeleton className="h-56 w-full my-2 rounded-xl" />
+                        </div>
+                        <div className="flex gap-4 mt-2 items-center">
+                            <Skeleton className="h-10 w-32 rounded-3xl" />
+                            <Skeleton className="h-8 w-8 rounded-full" />
                         </div>
                     </div>
                 ))}
