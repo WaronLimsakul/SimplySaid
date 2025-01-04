fc28256a2527663d04046b77273312e449b63398
Waron Limsakul <poonro@gmail.com>
2025-01-04 15:28:40 -0500

    Update error page

--- app/(root)/error.tsx
+++ app/(root)/error.tsx
@@ -5,42 +5,40 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 
 const Error = ({
-  error,
-  reset,
+    error,
+    reset,
 }: {
-  error: Error & { digest?: string };
-  reset: () => void;
+    error: Error & { digest?: string };
+    reset: () => void;
 }) => {
-  useEffect(() => {
-    console.log("From root/ ", error);
-  }, [error]);
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="text-center p-8 bg-white rounded-lg shadow-md max-w-md">
-        <h1 className="text-4xl font-bold text-destructive-foreground mb-4">
-          Oops!
-        </h1>
-        <p className="text-gray-700 text-lg mb-6">
-          Something went wrong. We couldn't find the page you were looking for.
-        </p>
-        <div className="flex justify-center gap-4">
-          <Link href="/">
-            <Button className="px-4 py-2 bg-secondary text-white rounded hover:bg-green-800">
-              Go Home
-            </Button>
-          </Link>
-          <Button
-            onClick={() => {
-              reset();
-            }}
-            className="px-4 py-2 bg-secondary text-white rounded hover:bg-green-800"
-          >
-            Try again
-          </Button>
+    useEffect(() => {
+        console.log("From root/ ", error);
+    }, [error]);
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="text-center p-8 bg-white rounded-lg shadow-md max-w-md">
+                <h1 className="text-4xl font-bold text-zinc-500 mb-4">Oops!</h1>
+                <p className="text-gray-700 text-lg mb-6">
+                    Something went wrong. We couldn't find the page you were looking for.
+                </p>
+                <div className="flex justify-center gap-4">
+                    <Link href="/">
+                        <Button className="px-4 py-2 bg-secondary text-white rounded hover:bg-green-800">
+                            Go Home
+                        </Button>
+                    </Link>
+                    <Button
+                        onClick={() => {
+                            reset();
+                        }}
+                        className="px-4 py-2 bg-secondary text-white rounded hover:bg-green-800"
+                    >
+                        Try again
+                    </Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Error;
