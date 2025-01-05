7b796130cd305f6dc0c804bb1b88f03828e05a00
Waron Limsakul <poonro@gmail.com>
2025-01-05 18:12:11 -0500

    Update responsize preview to appear below

--- app/components/posting/WritingPostArea.tsx
+++ app/components/posting/WritingPostArea.tsx
@@ -8,39 +8,39 @@ import DOMPurify from "dompurify";
 import { Card, CardContent, CardDescription } from "@/components/ui/card";
 
 const WritingPostArea = () => {
-  const [content, setContent] = useState("");
-  const parser = new MdParser();
-  return (
-    <div className="grid grid-cols-11 h-[60vh]">
-      <div className="col-span-11 md:col-span-5">
-        <CardDescription>Write your explanation here:</CardDescription>
-        <Textarea
-          onChange={(e) => {
-            setContent(e.target.value);
-            console.log(parser.render(content));
-          }}
-          placeholder="*markdown language preview is enabled"
-          className="min-h-full"
-        />
-      </div>
-      <Separator
-        orientation="vertical"
-        className="mx-auto md:col-span-1 hidden md:block"
-      />
-      <div className="hidden md:col-span-5 md:block">
-        <CardDescription> See your explanation preview here:</CardDescription>
-        <Card className="min-h-full">
-          <CardContent className="my-3 w-auto h-full">
-            <div
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(parser.render(content)),
-              }}
+    const [content, setContent] = useState("");
+    const parser = new MdParser();
+    return (
+        <div className="grid grid-cols-11 h-[60vh]">
+            <div className="col-span-11 md:col-span-5">
+                <CardDescription>Write your explanation here:</CardDescription>
+                <Textarea
+                    onChange={(e) => {
+                        setContent(e.target.value);
+                        console.log(parser.render(content));
+                    }}
+                    placeholder="*markdown language preview is enabled"
+                    className="min-h-full"
+                />
+            </div>
+            <Separator
+                orientation="vertical"
+                className="mx-auto md:col-span-1 hidden md:block"
             />
-          </CardContent>
-        </Card>
-      </div>
-    </div>
-  );
+            <div className="col-span-11 mt-8 md:mt-0 md:col-span-5 md:block">
+                <CardDescription> See your explanation preview here:</CardDescription>
+                <Card className="min-h-full">
+                    <CardContent className="my-3 w-auto h-full">
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(parser.render(content)),
+                            }}
+                        />
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
 };
 
 export default WritingPostArea;
