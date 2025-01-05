bc9bd9da347977c2a24e2f5c01d347fe389f5437
Waron Limsakul <poonro@gmail.com>
2025-01-05 16:19:51 -0500

    Update posting button rendering condition

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,18 @@
+import { auth } from "@/auth";
 import Navbar from "../components/navbar/Navbar";
 import PostingButton from "../components/posting/PostingButton";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <main>
-            <Navbar />
-            {children}
-            <PostingButton />
-        </main>
-    );
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  return (
+    <main>
+      <Navbar />
+      {children}
+      {session && <PostingButton />}
+    </main>
+  );
 }
