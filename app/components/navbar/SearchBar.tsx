07372cbddb353098ee7c48f155d83781fe36da96
Waron Limsakul <poonro@gmail.com>
2025-01-06 17:33:29 -0500

    Update a tooltip and small view port search bar

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -3,95 +3,97 @@ import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { useRouter } from "next/navigation";
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from "@/components/ui/tooltip";
 
 const filters = [
-    { name: "Fuzzy Search (DIY)", value: "fuzzy" },
-    { name: "Atlas Search", value: "atlas" },
-    { name: "Topic (exact)", value: "object" },
-    { name: "Tags (exact)", value: "tags" },
-    { name: "UserID (exact)", value: "user_id" },
+  { name: "Fuzzy Search (DIY)", value: "fuzzy" },
+  { name: "Atlas Search", value: "atlas" },
+  { name: "Topic (exact)", value: "object" },
+  { name: "Tags (exact)", value: "tags" },
+  { name: "UserID (exact)", value: "user_id" },
 ];
 
 // dev validate searchBy before searching.
 const SearchBar = () => {
-    const [searchBy, setSearchBy] = useState("fuzzy");
-    const [query, setQuery] = useState("");
-    const router = useRouter();
+  const [searchBy, setSearchBy] = useState("fuzzy");
+  const [query, setQuery] = useState("");
+  const router = useRouter();
 
-    const search = () => {
-        router.push(
-            `${process.env.NEXT_PUBLIC_CLIENT_URI}/post?${searchBy}=${query}`,
-        );
-    };
-    // use "onValueChange" for handle state with <Select>
-    // use "onChange" for normal <input>
-    return (
-        <div className="flex w-full max-w-xl align-center justify-center">
-            <Select onValueChange={setSearchBy}>
-                <SelectTrigger className="m-1 w-1/3 border-primary-foreground text-primary-foreground border-2">
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <SelectValue
-                                    placeholder="Fuzzy Search (DIY)"
-                                    defaultValue={"fuzzy"}
-                                ></SelectValue>
-                            </TooltipTrigger>
-                            <TooltipContent className="bg-zinc-200 text-zinc-900">
-                                <p>Select a method to search post</p>
-                                <p>
-                                    Read more on{" "}
-                                    <a
-                                        href="https://github.com/WaronLimsakul/SimplySaid"
-                                        className="text-red-700 text-bold italic"
-                                    >
-                                        doc
-                                    </a>
-                                </p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </SelectTrigger>
-                <SelectContent>
-                    {filters.map((filt) => (
-                        <SelectItem value={filt.value} key={filt.name}>
-                            {filt.name}
-                        </SelectItem>
-                    ))}
-                </SelectContent>
-            </Select>
-            <Input
-                type="text"
-                placeholder="💡Let's demystify..."
-                className="bg-zinc-50 m-1"
-                onChange={(e) => {
-                    setQuery(e.target.value);
-                }}
-                onKeyDown={(event) => {
-                    if (event.key == "Enter") search();
-                }}
-            />
-            <Button
-                onClick={search}
-                className="m-1 bg-white text-zinc-800 hover:bg-gray-200"
-            >
-                Search
-            </Button>
-        </div>
+  const search = () => {
+    router.push(
+      `${process.env.NEXT_PUBLIC_CLIENT_URI}/post?${searchBy}=${query}`,
     );
+  };
+  // use "onValueChange" for handle state with <Select>
+  // use "onChange" for normal <input>
+  return (
+    <div className="flex w-full max-w-xl align-center justify-center">
+      <Select onValueChange={setSearchBy}>
+        <SelectTrigger className="m-1 w-1/3 border-primary-foreground text-primary-foreground border-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <div>
+                  <SelectValue
+                    placeholder="Fuzzy Search (DIY)"
+                    defaultValue={"fuzzy"}
+                  />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent className="bg-zinc-200 text-zinc-900">
+                <p>Select a method to search post</p>
+                <p>
+                  Read more on{" "}
+                  <a
+                    href="https://github.com/WaronLimsakul/SimplySaid"
+                    className="text-red-700 text-bold italic"
+                  >
+                    doc
+                  </a>
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </SelectTrigger>
+        <SelectContent>
+          {filters.map((filt) => (
+            <SelectItem value={filt.value} key={filt.name}>
+              {filt.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Input
+        type="text"
+        placeholder="💡Let's demystify..."
+        className="bg-zinc-50 m-1"
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        onKeyDown={(event) => {
+          if (event.key == "Enter") search();
+        }}
+      />
+      <Button
+        onClick={search}
+        className="m-1 bg-white text-zinc-800 hover:bg-gray-200"
+      >
+        Search
+      </Button>
+    </div>
+  );
 };
 
 export default SearchBar;
--- app/components/navbar/SmallViewPortSearchBar.tsx
+++ app/components/navbar/SmallViewPortSearchBar.tsx
@@ -2,54 +2,61 @@
 
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { Ref, useRef, useState } from "react";
 
 const SmallViewPortSearchBar = () => {
-    const [query, setQuery] = useState("");
-    const router = useRouter();
+  const [query, setQuery] = useState("");
+  // we access DOM by using ref.
+  const hiddenCloseButton: Ref<HTMLButtonElement> = useRef(null);
+  const router = useRouter();
 
-    // No query choice for mobile
-    const search = () => {
-        router.push(`${process.env.NEXT_PUBLIC_CLIENT_URI}/post?fuzzy=${query}`);
-    };
-    return (
-        <div className="md:hidden flex items-center">
-            <Dialog>
-                <DialogTrigger>
-                    <Search color="white" />
-                </DialogTrigger>
-                <DialogContent className="w-5/6 mx-auto top-[30%]">
-                    <DialogHeader>
-                        <DialogTitle>Search explanation</DialogTitle>
-                        <DialogDescription className="items-start">
-                            Using keyword search for simple explanation by fuzzy search.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <Input
-                        type="text"
-                        placeholder="💡Let's demystify..."
-                        className="px-auto py-auto rounded-md bg-zinc-50"
-                        onChange={(e) => {
-                            setQuery(e.target.value);
-                        }}
-                        onKeyDown={(e) => {
-                            if (e.key == "Enter") search();
-                        }}
-                    />
-                </DialogContent>
-            </Dialog>
-        </div>
-    );
+  // No query choice for mobile
+  const search = () => {
+    router.push(`${process.env.NEXT_PUBLIC_CLIENT_URI}/post?fuzzy=${query}`);
+    hiddenCloseButton.current?.click();
+  };
+  return (
+    <div className="md:hidden flex items-center">
+      <Dialog>
+        <DialogTrigger>
+          <Search color="white" />
+        </DialogTrigger>
+        <DialogContent className="w-5/6 mx-auto top-[30%]">
+          <DialogHeader>
+            <DialogTitle>Search explanation</DialogTitle>
+            <DialogDescription className="items-start">
+              Using keyword search for simple explanation by fuzzy search.
+            </DialogDescription>
+          </DialogHeader>
+          <Input
+            type="text"
+            placeholder="💡Let's demystify..."
+            className="px-auto py-auto rounded-md bg-zinc-50"
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") search();
+            }}
+          />
+          <DialogClose className="hidden" asChild>
+            <button ref={hiddenCloseButton} />
+          </DialogClose>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default SmallViewPortSearchBar;
