8f3603e8d6d3c3876d8c7883a7f49f0d7472b470
Waron Limsakul <poonro@gmail.com>
2025-01-07 10:19:31 -0500

    Add error messsage in toast

--- app/components/posting/PostingCardBody.tsx
+++ app/components/posting/PostingCardBody.tsx
@@ -8,58 +8,61 @@ import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
 
 const PostingCardBody = () => {
-    const { toast } = useToast();
-    const router = useRouter();
-    const [content, setContent] = useState("");
-    const [title, setTitle] = useState("");
-    const [object, setObject] = useState("");
-    const [tags, setTags] = useState<string[]>([]);
-    const [posting, setPosting] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [object, setObject] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
+  const [posting, setPosting] = useState(false);
 
-    const handlePost: FormEventHandler<HTMLFormElement> = async (e) => {
-        e.preventDefault();
-        if (!content || !title || !tags || !object) {
-            toast({ title: "Please fill all the post detail", symbol: "fail" });
-            return;
-        }
-        setPosting(true);
-        const result = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/post`, {
-            method: "POST",
-            body: JSON.stringify({ post: { object, title, tags, content } }),
-        });
+  const handlePost: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    if (!content || !title || !tags || !object) {
+      toast({ title: "Please fill all the post detail", symbol: "fail" });
+      return;
+    }
+    setPosting(true);
+    const result = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/post`, {
+      method: "POST",
+      body: JSON.stringify({ post: { object, title, tags, content } }),
+    });
 
-        if (result.ok) {
-            toast({ title: "Posting Successful", symbol: "check" });
-            // may use server redirect. let's see.
-            router.push("/");
-        } else toast({ title: "Posting Fail, please try again", symbol: "fail" });
+    if (result.ok) {
+      toast({ title: "Posting Successful", symbol: "check" });
+      // may use server redirect. let's see.
+      router.push("/");
+    } else {
+      const errorMessage = result.text;
+      toast({ title: `Posting Fail, Error:${errorMessage}`, symbol: "fail" });
+    }
 
-        setPosting(false);
-    };
+    setPosting(false);
+  };
 
-    return (
-        <div>
-            <CardContent>
-                <WritingPostArea
-                    content={content}
-                    setContent={setContent}
-                    title={title}
-                    setTitle={setTitle}
-                    object={object}
-                    setObject={setObject}
-                    tags={tags}
-                    setTags={setTags}
-                />
-            </CardContent>
-            <CardFooter className="justify-end">
-                <form onSubmit={handlePost}>
-                    <Button disabled={posting} className="mt-2" type="submit">
-                        Post
-                    </Button>
-                </form>
-            </CardFooter>
-        </div>
-    );
+  return (
+    <div>
+      <CardContent>
+        <WritingPostArea
+          content={content}
+          setContent={setContent}
+          title={title}
+          setTitle={setTitle}
+          object={object}
+          setObject={setObject}
+          tags={tags}
+          setTags={setTags}
+        />
+      </CardContent>
+      <CardFooter className="justify-end">
+        <form onSubmit={handlePost}>
+          <Button disabled={posting} className="mt-2" type="submit">
+            Post
+          </Button>
+        </form>
+      </CardFooter>
+    </div>
+  );
 };
 
 export default PostingCardBody;
--- app/writing/loading.tsx
+++ app/writing/loading.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { LoadingSpinner } from "../components/feed/LoadingSpinner";
 
 const loading = () => {
-  return (
-    <div className="flex w-screen h-screen justify-center items-center">
-      <LoadingSpinner size={60} color="#ff6f00" />
-    </div>
-  );
+    return (
+        <div className="flex w-screen h-screen justify-center items-center">
+            <LoadingSpinner size={60} color="#ff6f00" />
+        </div>
+    );
 };
 
 export default loading;
